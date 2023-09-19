a5faecd4f357ae4be06496547b11d3f7b6a7bfe3
Karlo P <kpericic@student.unipu.hr>
2023-09-19 07:18:08 +0200

    Add login + register

---
+++ src/views/LoginView.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: "LoginView",
+    data() {
+        return {
+            dropzoneOptions: {
+              url: this.$store.getters.getUploadURL,
+              thumbnailWidth: 150
+            }
+      }
+    },
+    watch: {
+        filter(value) {
+            this.updateActiveFilter(value);
+        },
+    },
+};
+</script>
+
+<template>
+<div class="wrapper">
+    This is login
+    <router-link to="/register">Register</router-link>
+</div>
+</template>
+
+<style scoped>
+.wrapper  {
+    width: 100%;
+    height: 100vh;
+}
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: "LoginView",
+    data() {
+        return {
+            dropzoneOptions: {
+              url: this.$store.getters.getUploadURL,
+              thumbnailWidth: 150
+            }
+      }
+    },
+    watch: {
+        filter(value) {
+            this.updateActiveFilter(value);
+        },
+    },
+};
+</script>
+
+<template>
+<div class="wrapper">
+    This is register
+    <router-link to="/login">Have an account?</router-link>
+</div>
+</template>
+
+<style scoped>
+.wrapper  {
+    width: 100%;
+    height: 100vh;
+}
+</style>
