2c5428de671aad9b585440dfda937a91ae4732f3
Karlo P <kpericic@student.unipu.hr>
2023-09-19 07:17:55 +0200

    Update App

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
-<script setup>
+<script>
+import { mapGetters } from "vuex";
 import UserInfo from './components/UserInfo.vue'
+
+export default {
+    computed: {
+        ...mapGetters(["isAuthenticated"]),
+    },
+};
 </script>
 
 <template>
   <div id="app">
-    <header>
-      <div class="profile-img">P</div>
+    <div v-if="isAuthenticated" id="auth-layout">
+      <header>
+        <div class="profile-img">P</div>
 
-      <div class="wrapper">
-          <UserInfo username="Username" />
-          <!-- <nav>
-              <router-link to="/">Home</router-link>
-              <router-link to="/about">About</router-link>
-          </nav> -->
-      </div>
-    </header>
+        <div class="wrapper">
+            <UserInfo username="Username" />
+            <!-- <nav>
+                <router-link to="/">Home</router-link>
+                <router-link to="/about">About</router-link>
+            </nav> -->
+        </div>
+      </header>
 
-    <router-view />
+      <router-view />
+    </div>
+
+    <router-view v-else />
   </div>
 </template>
 
