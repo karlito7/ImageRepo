64d44cdcfcc4c51ecd28abe7e4b6b8274baf0b94
Karlo P <kpericic@student.unipu.hr>
2023-09-19 07:17:43 +0200

    Add public routed + guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -11,14 +12,43 @@ const router = new VueRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+      meta: { requiresAuth: false }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue'),
+      meta: { requiresAuth: false }
     },
     {
       path: '/upload-image',
       name: 'uploadImage',
-      component: () => import('../views/UploadImage.vue')
+      component: () => import('../views/UploadImage.vue'),
+      meta: { requiresAuth: true }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+
+    if (!store.getters.isAuthenticated) {
+      next({ name: 'login' })
+    }
+    else {
+      next()
+    }
+  }
+  else {
+    next()
+  }
 })
 
 export default router
